5d3a92b1d9797140a84b45524434db76791d5275
mahmut-berat <mahmutberatimdat@gmail.com>
2024-11-23 13:08:27 +0300

    Remove outdated exception and validation classes

--- src/main/java/com/mbi/study/exception/LoanNotEnoughLimitException.java
+++
@@ -1,7 +0,0 @@
-package com.mbi.study.exception;
-
-public class LoanNotEnoughLimitException extends RuntimeException {
-    public LoanNotEnoughLimitException(String string) {
-        super(string);
-    }
-}
